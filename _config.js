ee936c649734004a077bd5cb48c5c5765eb7ab18
Mildred <mildredjepkosgei@gmail.com>
2023-03-30 18:47:33 +0300

    initial commit

---
+++ _config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://mildredkulei:FecKLHABllwiAEZz@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://mildredkulei:FecKLHABllwiAEZz@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://mildredkulei:FecKLHABllwiAEZz@mildredip.ks0xtkk.mongodb.net/?retryWrites=true&w=majority',
+}
+module.exports = config;
